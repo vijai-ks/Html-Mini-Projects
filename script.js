1a4da0d4e1556dcfc46ecb70b4bd72c6bb49d91d
vijai-ks <vijai.ksv@gmail.com>
2023-05-14 23:24:30 +0530

    feat: signup: add signup page
    Signed-off-by: vijai-ks <vijai.ksv@gmail.com>

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const loginForm = document.querySelector(".login-form");
+// DOM Elements
+
+const loginForm = document.querySelector(".loginForm");
 const mainLoginButton = document.querySelector(".login-button");
+const registerForm = document.querySelector(".register-form");
+const mainRegisterButton = document.querySelector(".register-button");
 
 mainLoginButton.addEventListener("click", () => {
   loginForm.classList.add("login-form-box");
+});
+
+mainRegisterButton.addEventListener("click", () => {
+  loginForm.classList.remove("login-form-box");
+  registerForm.classList.add("register-form-box");
 });
